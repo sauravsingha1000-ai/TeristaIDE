401f191a11457efa565e219c4059a38fb3dcb18e
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:10:07 +0530

    Update build.gradle.kts

--- subprojects/tooling-api/build.gradle.kts
+++ subprojects/tooling-api/build.gradle.kts
@@ -5,14 +5,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 @Suppress("JavaPluginLanguageLevel")
@@ -21,13 +13,19 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
     api(projects.buildInfo)
     api(projects.logger)
     api(projects.shared)
     api(projects.subprojects.toolingApiModel)
     api(projects.subprojects.toolingApiEvents)
-    
+
     api(libs.google.gson)
     api(libs.common.jsonrpc)
     implementation(libs.common.jkotlin)
