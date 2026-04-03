16f4e2d4996012b988adc2b1b6dccd3093e10252
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:12:25 +0530

    Update build.gradle.kts

--- subprojects/tooling-api-model/build.gradle.kts
+++ subprojects/tooling-api-model/build.gradle.kts
@@ -5,20 +5,18 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 @Suppress("JavaPluginLanguageLevel")
 plugins {
-  id ("java-library")
-  id ("org.jetbrains.kotlin.jvm")
+  id("java-library")
+  id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
 }
 
 dependencies {
@@ -31,15 +29,15 @@ dependencies {
   implementation(libs.common.jkotlin)
 }
 
-tasks.register < Copy > ("copyToTestDir") {
-  from (project.layout.buildDirectory.file("libs/tooling-api-model.jar"))
-  into (project.rootProject.mkdir ("tests/test-home/.androidide/init"))
+tasks.register<Copy>("copyToTestDir") {
+  from(project.layout.buildDirectory.file("libs/tooling-api-model.jar"))
+  into(project.rootProject.mkdir("tests/test-home/.androidide/init"))
   rename { "model.jar" }
 
   outputs.upToDateWhen { false }
 }
 
-project.tasks.jar {
-  finalizedBy ("copyToTestDir")
+tasks.named<Jar>("jar") {
+  finalizedBy("copyToTestDir")
   outputs.upToDateWhen { false }
 }
