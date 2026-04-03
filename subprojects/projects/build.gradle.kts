8a861d4c3ceafaef037d9696b252b9a1efa17dcd
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:16:20 +0530

    Update build.gradle.kts

--- subprojects/projects/build.gradle.kts
+++ subprojects/projects/build.gradle.kts
@@ -7,6 +7,17 @@ plugins {
 
 android {
   namespace = "${BuildConfig.packageName}.projects"
+
+  compileSdk = 35
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  kotlinOptions {
+    jvmTarget = "17"
+  }
 }
 
 kapt {
