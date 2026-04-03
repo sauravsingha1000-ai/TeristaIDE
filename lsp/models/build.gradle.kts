22cdc24460910b46ae1646c5455c81260496ed83
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:00:41 +0530

    Update build.gradle.kts

--- lsp/models/build.gradle.kts
+++ lsp/models/build.gradle.kts
@@ -5,13 +5,24 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.lsp.models"
+
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation(libs.common.editor)
     implementation(projects.common)
     implementation(projects.subprojects.fuzzysearch)
-    
+
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.common.kotlin)
