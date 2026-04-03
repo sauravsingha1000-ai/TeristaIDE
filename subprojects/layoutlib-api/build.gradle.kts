c86321af6974018fd5ecb87870ae45e88ef1c5ef
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:17:22 +0530

    Update build.gradle.kts

--- subprojects/layoutlib-api/build.gradle.kts
+++ subprojects/layoutlib-api/build.gradle.kts
@@ -5,15 +5,8 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -21,22 +14,33 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.layoutlib"
-    
+
+    compileSdk = 35
+
     buildTypes {
         release {
             isMinifyEnabled = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation(libs.common.jkotlin)
-    
+
     implementation(libs.aapt2.annotations)
     implementation(libs.aapt2.jb.annotations)
-    
+
     implementation(libs.google.guava)
-    
+
     testImplementation(libs.tests.junit)
     testImplementation(libs.tests.google.truth)
 }
