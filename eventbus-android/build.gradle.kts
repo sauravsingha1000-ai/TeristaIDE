b1b0f954fb57f0c5482d03ebddb2e42d73b9d041
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 00:35:26 +0530

    Update build.gradle.kts

--- eventbus-android/build.gradle.kts
+++ eventbus-android/build.gradle.kts
@@ -21,6 +21,17 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.eventbus.android"
+
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
