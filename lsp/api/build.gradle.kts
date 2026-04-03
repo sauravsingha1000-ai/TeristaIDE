bbd8e1ffb12417d45bec13756db71127a7b521dc
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:02:33 +0530

    Update build.gradle.kts

--- lsp/api/build.gradle.kts
+++ lsp/api/build.gradle.kts
@@ -5,14 +5,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 plugins {
@@ -23,34 +15,45 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.lsp.api"
+
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 kapt {
     arguments {
-        arg ("eventBusIndex", "${BuildConfig.packageName}.events.LspApiEventsIndex")
+        arg("eventBusIndex", "${BuildConfig.packageName}.events.LspApiEventsIndex")
     }
 }
 
 dependencies {
-    
+
     kapt(projects.annotationProcessors)
-    
+
     implementation(libs.common.editor)
     implementation(projects.subprojects.fuzzysearch)
     implementation(projects.eventbusEvents)
-    
+
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.common.kotlin)
     implementation(libs.common.utilcode)
     implementation(libs.google.material)
-    
+
     api(projects.subprojects.projects)
     api(projects.subprojects.xmlUtils)
     api(projects.lookup)
     api(projects.lsp.models)
     api(projects.preferences)
-    
+
     compileOnly(projects.actions)
     compileOnly(projects.common)
 }
