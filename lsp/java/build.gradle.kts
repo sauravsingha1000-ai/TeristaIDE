afaaa5dc045e74f4fc045017b82f3faa46166c6c
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 01:01:46 +0530

    Update build.gradle.kts

--- lsp/java/build.gradle.kts
+++ lsp/java/build.gradle.kts
@@ -5,15 +5,8 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -22,18 +15,29 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.lsp.java"
+
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 kapt {
     arguments {
-        arg ("eventBusIndex", "${BuildConfig.packageName}.events.LspJavaEventsIndex")
+        arg("eventBusIndex", "${BuildConfig.packageName}.events.LspJavaEventsIndex")
     }
 }
 
 dependencies {
-    
+
     kapt(projects.annotationProcessors)
-    
+
     implementation(libs.androidide.ts)
     implementation(libs.androidide.ts.java)
     implementation(libs.common.editor)
@@ -42,12 +46,12 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.google.guava)
     implementation(libs.google.gson)
-    
+
     compileOnly(libs.androidx.appcompat)
     compileOnly(libs.google.material)
     compileOnly(projects.actions)
     compileOnly(projects.common)
-    
+
     implementation(projects.editorApi)
     implementation(projects.resources)
     implementation(projects.lsp.api)
@@ -55,7 +59,7 @@ dependencies {
     implementation(projects.subprojects.javacServices)
     implementation(projects.subprojects.javapoet)
     implementation(projects.subprojects.googleJavaFormat)
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.common.kotlin)
 
