4c9cd4db8d724316a55b968425aa3fbd1f07d0a2
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 12:11:25 +0530

    Update build.gradle.kts

--- subprojects/framework-stubs/build.gradle.kts
+++ subprojects/framework-stubs/build.gradle.kts
@@ -5,14 +5,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 @Suppress("JavaPluginLanguageLevel")
@@ -20,6 +12,12 @@ plugins {
     id("java-library")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
-    compileOnlyApi(files ("libs/android.jar"))
+    compileOnlyApi(files("libs/android.jar"))
 }
