b0ada770f621e4ab09947cd0fc2b76091f10c2d8
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 12:44:27 +0530

    Update build.gradle.kts

--- testing/lsp/build.gradle.kts
+++ testing/lsp/build.gradle.kts
@@ -5,14 +5,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 plugins {
@@ -22,12 +14,23 @@ plugins {
 
 android {
     namespace = "${BuildConfig.packageName}.lsp.testing"
+
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     api(libs.androidx.core.ktx)
     api(libs.common.kotlin)
-    
+
     api(projects.lsp.api)
     api(projects.actions)
     api(projects.eventbusAndroid)
