9eb24830146d0b6b2319d96f227b7213c63bacf5
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-04 12:24:31 +0530

    Update build.gradle.kts

--- testing/unit/build.gradle.kts
+++ testing/unit/build.gradle.kts
@@ -5,14 +5,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 plugins {
@@ -22,6 +14,17 @@ plugins {
 
 android {
   namespace = "${BuildConfig.packageName}.testing.unit"
+
+  compileSdk = 35
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  kotlinOptions {
+    jvmTarget = "17"
+  }
 }
 
 dependencies {
