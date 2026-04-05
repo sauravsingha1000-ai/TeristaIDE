eb998e4cd883413e87d5f76c861196c7cc87a254
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-04-06 00:15:33 +0530

    Update URIResolverExtension.java

--- subprojects/xml-dom/src/main/java/org/eclipse/lemminx/uriresolver/URIResolverExtension.java
+++ subprojects/xml-dom/src/main/java/org/eclipse/lemminx/uriresolver/URIResolverExtension.java
@@ -11,31 +11,32 @@ package org.eclipse.lemminx.uriresolver;
 
 import java.io.IOException;
 
-import jaxp.sun.org.apache.xerces.internal.xni.XMLResourceIdentifier;
-import jaxp.sun.org.apache.xerces.internal.xni.XNIException;
-import jaxp.sun.org.apache.xerces.internal.xni.parser.XMLEntityResolver;
-import jaxp.sun.org.apache.xerces.internal.xni.parser.XMLInputSource;
+import org.apache.xerces.xni.XMLResourceIdentifier;
+import org.apache.xerces.xni.XNIException;
+import org.apache.xerces.xni.parser.XMLEntityResolver;
+import org.apache.xerces.xni.parser.XMLInputSource;
 
 /** URI resolver API */
 public interface URIResolverExtension extends XMLEntityResolver {
 
-  public static final String DEFAULT = "default";
+public static final String DEFAULT = "default";
 
-  default String getName() {
-    return getClass().getSimpleName();
-  }
+default String getName() {
+return getClass().getSimpleName();
+}
 
-  /**
-   * @param baseLocation - the location of the resource that contains the uri
-   * @param publicId - an optional public identifier (i.e. namespace name), or null if none
-   * @param systemId - an absolute or relative URI, or null if none
-   * @return an absolute URI representation of the 'logical' location of the resource
-   */
+/**
+
+* @param baseLocation - the location of the resource that contains the uri
+* @param publicId - an optional public identifier (i.e. namespace name), or null if none
+* @param systemId - an absolute or relative URI, or null if none
+* @return an absolute URI representation of the 'logical' location of the resource
+  */
   public String resolve(String baseLocation, String publicId, String systemId);
 
-  @Override
-  default XMLInputSource resolveEntity(XMLResourceIdentifier resourceIdentifier)
-      throws XNIException, IOException {
-    return null;
-  }
+@Override
+default XMLInputSource resolveEntity(XMLResourceIdentifier resourceIdentifier)
+throws XNIException, IOException {
+return null;
+}
 }
